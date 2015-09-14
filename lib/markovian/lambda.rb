f416cdcdba5c6fdd3746e933ccd2d804f9e6805a
Alex Koppel <alex@alexkoppel.com>
2015-09-14 18:00:58 -0500

    Add gem root path convenience method

--- lib/markovian/lambda.rb
+++ lib/markovian/lambda.rb
@@ -1,7 +1,11 @@
+require 'markovian'
 require "markovian/lambda/version"
+require "markovian/lambda/tweeter"
 
 module Markovian
   module Lambda
-    # Your code goes here...
+    def self.root
+      Gem::Specification.find_by_name("markovian-lambda").gem_dir
+    end
   end
 end

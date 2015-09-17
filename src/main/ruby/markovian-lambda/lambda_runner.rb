98ace684be237684eed219649f1e44957a42761c
Alex Koppel <alex@alexkoppel.com>
2015-09-17 17:51:03 -0500

    Get compiling with Maven

--- src/main/ruby/markovian-lambda/lambda_runner.rb
+++ src/main/ruby/markovian-lambda/lambda_runner.rb
@@ -1,18 +1,24 @@
 require 'markovian/lambda'
 require 'java'
 
-java_package 'markovian-lambda'
+java_package 'markovian.lambda'
 
 java_import 'com.amazonaws.services.lambda.runtime.Context'
 java_import 'com.amazonaws.services.lambda.runtime.LambdaLogger'
 
 module LambdaRunner
+  java_signature 'void main()'
+  def self.main()
+    puts "Hello!"
+  end
+
   java_signature 'String handler(Map eventData, Context context)'
   # If the gods of srand so decree, post a tweet in response to a quarter-hourly ping.
   def handler(event_data, context)
+    event_data = JSON.parse(event_data_string)
     # We want to post a little more frequently than once a day, so we use a threshold
     # corresponding to 4 pings an hour, success roughly every 23 hours.
-    logger = context.getLogger
+    logger = Logger.new(STDOUT)
     logger.log("In message!")
     logger.log("Received event data #{event_data.inspect}")
     threshold = 1/(23 * 4).to_f
@@ -28,3 +34,4 @@ module LambdaRunner
     end
   end
 end
+LambdaRunner.become_java!
